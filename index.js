378eb0931296b8f2899d218096d88d6c7827a44b
Kalvin <kalvinarts@gmail.com>
2018-08-22 20:02:24 +0200

    Fixed export of the hex validator

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ module.exports = {
   alphanumeric: require('./lib/alphanumeric'),
   alphanumericExtra: require('./lib/alphanumericExtra'),
   email: require('./lib/email'),
+  hex: require('./lib/hex'),
 };
